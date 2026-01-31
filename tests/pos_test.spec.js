4bf41de76361cb7b1396a875410947d3c09944ec
charukaranesinhe2003 <charukaranesinhe2003@gmail.com>
2026-01-31 11:31:32 +0530

    next

--- tests/pos_test.spec.js
+++ tests/pos_test.spec.js
@@ -18,8 +18,270 @@ test('Pos_Fun_0002',async ({ page }) => {
   const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
 
   await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('');
+  
+});
+
+test('Pos_Fun_0003',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('');  
+});
+
+test('Pos_Fun_0004',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
   await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
   
 });
 
+test('Pos_Fun_0005',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0006',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0007',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0008',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0009',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0010',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0011',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0012',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0013',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0014',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0015',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0016',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0017',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0018',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0019',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0020',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0021',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0022',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0023',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0024',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0025',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
+
+test('Pos_Fun_0026',async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  const inputBox = page.locator('textarea');
+  const outputBox = page.locator('div.whitespace-pre-wrap.overflow-y-auto.flex-grow.bg-slate-50');
+
+  await inputBox.fill('oyaata kohomadha?');
+  await expect(outputBox).toContainText('මචන් මට අද meeting එකේ Zoom link එක email එකක් විදිහට එවන්න පුළුවන්ද? Please send it before 3pm. මම office යන්න කලින් check කරන්න ඕනේ. Email එකක් එවන්න අමාරුනම් WhatsApp ම්ස්ග් එකක් දාපන්. ථx!');
+  
+});
 
